3063441effbd4a9d9f9379c94826702ac563e1b7
XenoAmess <xenoamess@gmail.com>
2020-02-26 01:00:37 +0800

    init

---
+++ clean_baidu.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Clean_Baidu
+// @namespace    None
+// @version      0.0.1
+// @description  干掉百度个人认为多余的内容，让百度看起来像一个搜索引擎该有的样子。
+// @author       XenoAmess
+// @match        http://www.baidu.com/*
+// @match        https://www.baidu.com/*
+// @match        http://m.baidu.com/*
+// @match        https://m.baidu.com/*
+// @run-at       document-end
+// @grant        none
+// @supportURL   https://github.com/XenoAmess/EnablePIP.git
+// ==/UserScript==
+var STRING_SELECTORS = [];
+STRING_SELECTORS[0] = "div#s_wrap > div#s_main > div#s_mancard_main";
+STRING_SELECTORS[1] = "div.blank-frame > div.center-content";
+STRING_SELECTORS[2] = "div.ad-block";
+STRING_SELECTORS[3] = "div.cr-content > div.FYB_RD";
+
+function doIt() {
+    for (var i = 0; i < STRING_SELECTORS.length; i++) {
+        $(STRING_SELECTORS[i]).remove();
+    }
+}
+
+(function () {
+    'use strict';
+    if (!window.jQuery) {
+        var oScript = document.createElement('script');
+        oScript.type = "text/javascript";
+        oScript.src = "//s1.hdslb.com/bfs/static/jinkela/long/js/jquery/jquery1.7.2.min.js";
+        document.head.appendChild(oScript);
+    }
+    doIt();
+})();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# CleanBaidu
+
+## Description描述
+干掉百度个人认为多余的内容，让百度看起来像一个搜索引擎该有的样子。
+(
+需使用[油猴](https://www.tampermonkey.net//)
+)
