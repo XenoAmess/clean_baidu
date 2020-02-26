7ffe1061830763dd73622077e3297730562a78af
XenoAmess <xenoamess@gmail.com>
2020-02-26 20:04:00 +0800

    添加预处理css

--- clean_baidu.js
+++ clean_baidu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clean_Baidu
 // @namespace    None
-// @version      0.0.6
+// @version      0.0.7
 // @description  干掉百度个人认为多余的内容，让百度看起来像一个搜索引擎该有的样子。
 // @author       XenoAmess
 // @match        http://www.baidu.com/*
@@ -10,7 +10,7 @@
 // @match        https://m.baidu.com/*
 // @match        http://wap.baidu.com/*
 // @match        https://wap.baidu.com/*
-// @run-at       document-end
+// @run-at       document-start
 // @grant        none
 // @supportURL   https://github.com/XenoAmess/EnablePIP.git
 // ==/UserScript==
@@ -24,10 +24,24 @@ STRING_SELECTORS[3] = "div.cr-content > div.FYB_RD";
 STRING_SELECTORS[4] = "div#content_right > div.bdvideo-entry";
 STRING_SELECTORS[5] = "a.index-banner.square-banner-bgicon";
 
+function preProcess() {
+    var cssString = "";
+    for (var i = 0; i < STRING_SELECTORS.length; i++) {
+        cssString += STRING_SELECTORS[i] + ' {display: none !important}\n';
+    }
+    var styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.rel = 'stylesheet';
+    styleSheet.innerText = cssString
+    document.head.appendChild(styleSheet)
+}
 
 function doIt() {
-    for (var i = 0; i < STRING_SELECTORS.length; i++) {
-        $(STRING_SELECTORS[i]).remove();
+    try {
+        for (var i = 0; i < STRING_SELECTORS.length; i++) {
+            $(STRING_SELECTORS[i]).remove();
+        }
+    } catch (err) {
     }
 }
 
@@ -39,6 +53,6 @@ function doIt() {
         oScript.src = "//s1.hdslb.com/bfs/static/jinkela/long/js/jquery/jquery1.7.2.min.js";
         document.head.appendChild(oScript);
     }
-    doIt();
+    preProcess();
     window.onload = window.setInterval(doIt, REFRESH_TIME);
 })();
